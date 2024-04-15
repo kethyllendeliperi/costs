d334c5a1ec7095e1dee216bc4f87a6ae909b89a4
Kethyllen Deliperi <kethyllenldeliperi@gmail.com>
2024-04-15 20:28:16 -0300

    Componentizando e estilizando o formulário

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,12 @@
-import Container from "../layout/Container"
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
 
 function NewProject() {
     return (
-        <div>
-            <h1>Novo projeto</h1>
+        <div className={styles.new_project_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto, para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto" />
         </div>
     )
 }
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,28 @@
+import Input from '../Form/Input'
+import Select from '../Form/Select'
+import SubmitButton from '../Form/SubmitButton'
+
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}) {
+    return (
+        <form className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento total do projeto"
+            />
+            <Select name="category_id" text="Selecione a categoria" />
+            <SubmitButton text={btnText}/>
+        </form>
+    )
+}
+
+export default ProjectForm
