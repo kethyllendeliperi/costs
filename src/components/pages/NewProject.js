cf86e935d4d7d1c13ef59bfee1b8cb6b9dfc4366
Kethyllen Deliperi <kethyllenldeliperi@gmail.com>
2024-04-16 15:48:17 -0300

    Inserindo feedback para o usuário após o cadastro do projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -18,13 +18,13 @@ function NewProject() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(project),
-        }).then((resp) => resp.json())
+        })
+            .then((resp) => resp.json())
             .then((data) => {
                 console.log(data)
-                navigate('projects', { message: 'Projeto criado com sucesso!' })
+                navigate('/projects', { state: { message: 'Projeto criado com sucesso!' } })
             })
             .catch(err => console.log(err))
-
     }
 
     return (
