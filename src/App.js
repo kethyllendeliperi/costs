86bf0181527fb1f07b4b100af735f6363289ea66
Kethyllen Deliperi <kethyllenldeliperi@gmail.com>
2024-04-17 10:09:53 -0300

    Edição de projetos finalizada

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Company from "./components/pages/Company"
 import Contact from "./components/pages/Contact"
 import NewProject from "./components/pages/NewProject"
 import Projects from "./components/pages/Projects"
+import Project from "./components/pages/Project"
 
 import Container from "./components/layout/Container"
 import Navbar from './components/layout/Navbar'
@@ -22,7 +23,8 @@ function App() {
         <Route path="projects" element= {< Projects />}/>
         <Route path="/company" element= {< Company />}/>
         <Route path="/contact" element= {< Contact />}/>
-        <Route path="newproject" element= {< NewProject />}/>
+        <Route path="/newproject" element= {< NewProject />}/>
+        <Route path="/project/:id" element= {< Project />}/>
       </Routes>
       </Container>
       <Footer/>
